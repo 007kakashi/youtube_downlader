899b3417baeb95a97d95140b09badeaac6d42cd6
007kakashi <upendrasgosavi@gmail.com>
2023-11-18 18:02:11 +0530

    local host downloading

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
-import pytube
-from pytube import Playlist
-from flask import Flask
+from constants import audio_downloader,video_downloader,playlist_downloader
+import streamlit as st
 import os
+import tkinter as tk
+from tkinter import filedialog
+
+def select_download_location():
+    root= tk.Tk()
+    root.withdraw()
+    folder_selected = filedialog.askdirectory()
+    return folder_selected
+
+def get_audio_download_link(file_path):
+    return f'<a href="{file_path}" download>Click here to download your audio</a>'
+
+def get_video_download_link(file_path):
+    return f'<a href="{file_path}" download>Click here to download your video</a>'
 
 
-def audio_downloader(url):
-    video=pytube.YouTube(url)
-    stream= video.streams.get_audio_only()
-    return stream.download(skip_existing=True)
+st.header('Youtube Video and Audio Downloader')
 
-def video_downloader(url):
-    video=pytube.YouTube(url)
-    stream= video.streams.get_highest_resolution()
-    return stream.download(skip_existing=True)
+user=st.text_input('Paste Your Link Here')
 
-def playlist_downloader(url):
-    playlist=Playlist(url)
-    for line in playlist.videos:
-        stream= line.streams.get_audio_only()
-    return stream.download(skip_existing=True)
+selection=st.selectbox('Please Select Format',['Video','Audio'])
+
+
+
+download=st.button("Download")
+
+if download and user:
+    if selection == 'Audio':
+        st.text("Download started for Audio...")
+        audio_file_path = audio_downloader(user)
+        st.text("Download completed for Audio!")
+        st.markdown(get_audio_download_link(audio_file_path), unsafe_allow_html=True)
+    else:
+        st.text("Download started for Video...")
+        video_file_path = video_downloader(user)
+        st.text("Download completed for Video!")
+        st.markdown(get_video_download_link(video_file_path), unsafe_allow_html=True)
+
---
+++ constant.py
@@ -0,0 +1,21 @@
+import pytube
+from pytube import Playlist
+from flask import Flask
+import os
+
+
+def audio_downloader(url):
+    video=pytube.YouTube(url)
+    stream= video.streams.get_audio_only()
+    return stream.download(skip_existing=True)
+
+def video_downloader(url):
+    video=pytube.YouTube(url)
+    stream= video.streams.get_highest_resolution()
+    return stream.download(skip_existing=True)
+
+def playlist_downloader(url):
+    playlist=Playlist(url)
+    for line in playlist.videos:
+        stream= line.streams.get_audio_only()
+    return stream.download(skip_existing=True)
